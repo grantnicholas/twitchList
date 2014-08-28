2140b0f8908bd597ec48b95b32f0ee004daf16d5
grantnicholas <grantnicholas2015@u.northwestern.edu>
2014-08-27 23:20:57 -0500

    twitchlist.py

--- twitchlist.py
+++ twitchlist.py
@@ -1,11 +1,20 @@
 import json
 import urllib2
+import os
+import re
 from pprint import pprint
+
+#A quick python script to grab the top live dota2 streams, display the streams in order of most views to least 
+#views, ask the user which stream they want to watch, and then open the stream from the terminal using
+#livestreamer. 
+#Just run python twitchlist.py
+#DEPENDENCY WARNING: livestreamer @ http://livestreamer.readthedocs.org/en/latest/install.html
+#for ubuntu: sudo apt-get install livestreamer
+
+  
 
 url = "https://api.twitch.tv/kraken/search/streams?q=dota2"
 data = json.load(urllib2.urlopen(url))
-
-#pprint(data)
 
 
 streams= data["streams"]
@@ -13,8 +22,13 @@ games = [];
 for i,var in enumerate(streams):
 	games.append((streams[i]["channel"]["views"],streams[i]["channel"]["status"].encode('ascii','ignore'),streams[i]["channel"]["url"].encode('ascii','ignore')))
 
-#print(games)
 games.sort(reverse=True)
 
-for var in games:
-	print "Views: " + str(var[0]) + " | Status: " + var[1] + " |Stream: " + var[2] + " \n"
+for i,var in enumerate(games):
+	print str(i) + " :|  Views: " + str(var[0]) + " | Status: " + var[1] + " |Stream: " + var[2] + " \n"
+
+numgame = input("Enter the number of the stream you wish to watch: ")
+regobj = re.search('twitch.tv/.*', games[numgame][2])
+gamelink = regobj.group(0)
+command = "livestreamer " + gamelink + " best"
+os.system(command)
